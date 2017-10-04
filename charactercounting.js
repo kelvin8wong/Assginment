62404a2740d742ef56fa4b18326442e0348f27da
Kelvin <kelvin8wong@gmail.com>
2017-10-04 20:01:41 +0000

    hand in assignment

---
+++ charactercounting.js
@@ -0,0 +1,14 @@
+function counterLetters (input){
+  var output = input.toLowerCase().split(' ').join('');
+  var counter = {};
+  for (var i = 0; i < output.length; i++) {
+    if (counter[output[i]] === undefined) {
+      counter[output[i]] = 1;
+    } else {
+      counter[output[i]] += 1;
+    }
+  }
+  return counter;
+}
+
+console.log (counterLetters("lighthouse in the house"));
